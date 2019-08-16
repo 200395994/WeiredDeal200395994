f001115d5f43f33edac0cb627003bdb8c70512a1
200395994 <43381169+200395994@users.noreply.github.com>
2019-08-16 12:03:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var age = document.getElementById("age");
+var locations = document.getElementById("locat");
+var email = document.getElementById("email");
+
+
+
+
+
+email.addEventListener("input", function (event) {
+
+   if(emial.validity.typeMismatch){
+    email.setCustomValidity("Enter@sample.com");
+} else {
+  email.setCustomValidity("");
+}
+});
+
+
+email.addEventListener("invalid", () => {
+
+  
+  if(email.value === ""){
+    email.setCustomValidity(" Enter");
+  } else {
+    email.setCustomValidity("");
+  }
+ 
+});
+
+
+
+
+firstName.addEventListener("invalid", () => {
+
+ 
+  if(firstName.value === ""){
+    firstName.setCustomValidity(" Please");
+  } else {
+    firstName.setCustomValidity("");
+  }
+ 
+});
+
+locations.addEventListener("invalid", () => {
+
+  
+  if(locations.value === ""){
+    locations.setCustomValidity(" Please");
+  } else {
+    locations.setCustomValidity("");
+  }
+ 
+});
+
+
+age.addEventListener("invalid", () => {
+
+  
+  if(age.value === ""){
+    age.setCustomValidity("Enter");
+  } else {
+    age.setCustomValidity("");
+  }
+ 
+});
+
